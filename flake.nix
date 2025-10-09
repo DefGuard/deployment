af1317fa5df5cbdf0d6859579970a7ea212b3ff8
Maciej Wójcik <maciej@defguard.net>
2025-10-09 12:13:47 +0200

    add nix setup

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Simple devshell flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          kubectl
+          kubernetes-helm
+          kubeconform
+        ];
+      };
+    });
+}
