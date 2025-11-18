56d9752cffc496a3328dd823ed361d99341bc0fb
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2025-11-18 11:31:33 +0100

    Update defguard-install.sh

--- cloudformation/ami/defguard-install.sh
+++ cloudformation/ami/defguard-install.sh
@@ -1,37 +1,37 @@
-#!/usr/bin/env bash
+#!/bin/sh
 set -e
 
 echo "Updating apt repositories..."
-apt update
+sudo apt update
 
 echo "Installing dependencies..."
-apt install -y ca-certificates curl awscli
+sudo apt install -y ca-certificates curl awscli
 
 echo "Adding Defguard GPG key..."
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://apt.defguard.net/defguard.asc -o /etc/apt/keyrings/defguard.asc
-chmod a+r /etc/apt/keyrings/defguard.asc
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://apt.defguard.net/defguard.asc -o /etc/apt/keyrings/defguard.asc
+sudo chmod a+r /etc/apt/keyrings/defguard.asc
 
 echo "Adding Defguard repository..."
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/defguard.asc] https://apt.defguard.net/ trixie release " | \
-   tee /etc/apt/sources.list.d/defguard.list > /dev/null
+   sudo tee /etc/apt/sources.list.d/defguard.list > /dev/null
 
 echo "Updating apt repositories after adding Defguard repo..."
-apt update
+sudo apt update
 
 echo "Installing Defguard packages with specific versions..."
 echo "  defguard version: ${CORE_VERSION}"
 echo "  defguard-proxy version: ${PROXY_VERSION}"
 echo "  defguard-gateway version: ${GATEWAY_VERSION}"
 
-apt install -y \
+sudo apt install -y \
   defguard=${CORE_VERSION} \
   defguard-proxy=${PROXY_VERSION} \
   defguard-gateway=${GATEWAY_VERSION}
 
-systemctl stop defguard
-systemctl disable defguard
-systemctl stop defguard-proxy
-systemctl disable defguard-proxy
-systemctl stop defguard-gateway
-systemctl disable defguard-gateway
+sudo systemctl stop defguard
+sudo systemctl disable defguard
+sudo systemctl stop defguard-proxy
+sudo systemctl disable defguard-proxy
+sudo systemctl stop defguard-gateway
+sudo systemctl disable defguard-gateway
