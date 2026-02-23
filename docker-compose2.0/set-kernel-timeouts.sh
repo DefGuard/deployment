0e0fea3050218769066b36c24ac0c3348c5ddc1e
Jacek Chmielewski <jacek@defguard.net>
2026-02-23 11:07:39 +0100

    cleanup

--- docker-compose2.0/set-kernel-timeouts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CID=docker-compose20-gateway-lb-1
-PID=$(docker inspect -f '{{.State.Pid}}' "$CID")
-
-sudo nsenter -t "$PID" -n sysctl -w net.netfilter.nf_conntrack_udp_timeout_stream=1
-sudo nsenter -t "$PID" -n sysctl -w net.netfilter.nf_conntrack_udp_timeout=1
