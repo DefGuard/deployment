98dbd068e4d8bc87a8f2f14058391a89c8b193b3
Jacek Chmielewski <jacek@defguard.net>
2026-09-22 10:13:44 +0200

    terraform fmt

--- terraform2.0/modules/openldap/outputs.tf
+++ terraform2.0/modules/openldap/outputs.tf
@@ -17,10 +17,10 @@ output "ldap_details" {
   description = "Connection details for the plain LDAP service"
   sensitive   = true
   value = {
-    address = aws_network_interface.openldap.private_ip
-    port    = var.ldap_port
-    domain  = var.ldap_domain
-    base_dn = "${join(",", [for part in split(".", var.ldap_domain) : "dc=${part}"])}"
+    address  = aws_network_interface.openldap.private_ip
+    port     = var.ldap_port
+    domain   = var.ldap_domain
+    base_dn  = "${join(",", [for part in split(".", var.ldap_domain) : "dc=${part}"])}"
     admin_dn = "cn=admin,${join(",", [for part in split(".", var.ldap_domain) : "dc=${part}"])}"
   }
 }
